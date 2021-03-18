b23b154759af27374fb47816310e177f3f7a7eed
IGI-111 <igi-111@protonmail.com>
2021-03-18 11:57:45 +0100

    Transfer file using webtorrent

--- src/download.ts
+++ src/download.ts
@@ -1,11 +1,14 @@
 import P2PT from 'p2pt';
+import WebTorrent from 'webtorrent';
 import trackers from './trackers';
 
 export class Download {
   public newCode = '';
   public code = undefined;
   private node;
-  public content = undefined;
+  public fileURI = undefined;
+  public progress = 0;
+  private leecher = new WebTorrent();
 
   public setCode(newCode: string) {
     this.code = newCode;
@@ -14,7 +17,20 @@ export class Download {
   private setupNode() {
     this.node = new P2PT(trackers, `cherami-${this.code}`);
     this.node.on('msg', (peer, msg) => {
-      this.content = msg;
+      const magnet = msg;
+
+      this.leecher.add(magnet, (torrent) => {
+        torrent.on('download', (bytes) => {
+          this.progress = torrent.progress;
+        });
+
+        torrent.on('done', () => {
+          this.progress = torrent.progress;
+          torrent.files[0].getBlobURL((err, url) => {
+            this.fileURI = url;
+          });
+        });
+      });
     });
     this.node.start();
   }
--- src/upload.ts
+++ src/upload.ts
@@ -1,24 +1,29 @@
 import P2PT from 'p2pt';
+import WebTorrent from 'webtorrent';
 import trackers from './trackers';
 import { randomWords } from './wordlist';
 
 export class Upload {
-  public newContent = '';
-  public content = undefined;
+  public selectedFiles: FileList;
+  private uploadingFiles: FileList;
   private node;
   private sent = false;
   private code = randomWords(4).join(' '); // this should only be the first part of the secret
 
-  public async setContent(newContent: string) {
-    this.content = newContent;
-    this.setupNode();
-  }
-  private setupNode() {
-    this.node = new P2PT(trackers, `cherami-${this.code}`);
-    this.node.on('peerconnect', (peer) => {
-      this.sent = true;
-      this.node.send(peer, this.content);
+  public upload(selectedFiles: FileList) {
+    this.uploadingFiles = selectedFiles;
+
+    const file = this.uploadingFiles.item(0);
+
+    const seeder = new WebTorrent();
+
+    seeder.seed(file, undefined, (torrent) => {
+      this.node = new P2PT(trackers, `cherami-${this.code}`);
+      this.node.on('peerconnect', (peer) => {
+        this.sent = true;
+        this.node.send(peer, torrent.magnetURI);
+      });
+      this.node.start();
     });
-    this.node.start();
   }
 }
